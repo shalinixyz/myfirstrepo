b8bff1e4f69a7ce687e6bb79716ab0d9c246eeb4
shalinixyz <rittishalini@gmail.com>
2018-04-09 15:10:55 +0530

    feature1 added

---
+++ BankApp/src/org/proj/BankApp/feature1.java
@@ -0,0 +1,11 @@
+package org.proj.BankApp;
+
+public class newclass {
+	public newclass() {
+		
+		System.out.println("new class contructor");
+		System.out.println("hi...modified");
+		// TODO Auto-generated constructor stub
+	}
+
+}
