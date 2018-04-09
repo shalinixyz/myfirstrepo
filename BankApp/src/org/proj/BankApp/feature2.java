fdacb9392f186f33bb291578186e363ca9f409aa
shalinixyz <rittishalini@gmail.com>
2018-04-09 16:07:43 +0530

    feature 2 modified

--- BankApp/src/org/proj/BankApp/feature2.java
+++ BankApp/src/org/proj/BankApp/feature2.java
@@ -4,7 +4,7 @@ public class feature2 {
 	public newclass() {
 		
 		
-		System.out.println("feature2 class");
+		System.out.println("feature2 class modified");
 		// TODO Auto-generated constructor stub
 	}
 
